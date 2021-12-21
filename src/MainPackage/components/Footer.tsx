9302a1b82803b2916b6644f5583fa3ad0c087a41
Leonid Veretennikov <megalok10@gmail.com>
2021-12-21 19:58:09 +0300

    Create Navigation component

--- src/MainPackage/components/Footer.tsx
+++ src/MainPackage/components/Footer.tsx
@@ -1,25 +1,16 @@
 import React, { ReactElement } from 'react';
-import { SectionsEnum } from '../enums/SectionsEnum';
 import { Logo } from './Logo';
-import { Link } from '../../UIKItPackage/components/Link';
-import { UnorderedList } from '../../UIKItPackage/components/UnorderedList';
-import { ListItem } from '../../UIKItPackage/components/ListItem';
+import { Navigation } from './Navigation';
 
 import styles from './Footer.pcss';
 
 export function Footer(): ReactElement {
-    const sections = Object.values(SectionsEnum).map(name => (
-        <ListItem key={name} showMarker={false}>
-            <Link name={name} href="#section" />
-        </ListItem>
-    ));
-
     return (
         <div className={styles.grid}>
             <Logo />
-            <nav className={styles.sections}>
-                <UnorderedList>{sections}</UnorderedList>
-            </nav>
+            <div className={styles.navigation}>
+                <Navigation />
+            </div>
         </div>
     );
 }
