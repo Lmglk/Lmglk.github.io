2e5b12c79be995e15e490453e4555305b884d3ea
Leonid Veretennikov <megalok10@gmail.com>
2021-12-21 18:30:13 +0300

    Change navigation styles

--- src/MainPackage/components/Footer.tsx
+++ src/MainPackage/components/Footer.tsx
@@ -2,18 +2,24 @@ import React, { ReactElement } from 'react';
 import { SectionsEnum } from '../enums/SectionsEnum';
 import { Logo } from './Logo';
 import { Link } from '../../UIKItPackage/components/Link';
+import { UnorderedList } from '../../UIKItPackage/components/UnorderedList';
+import { ListItem } from '../../UIKItPackage/components/ListItem';
 
 import styles from './Footer.pcss';
 
 export function Footer(): ReactElement {
     const sections = Object.values(SectionsEnum).map(name => (
-        <Link key={name} name={name} href="#section" />
+        <ListItem key={name} showMarker={false}>
+            <Link name={name} href="#section" />
+        </ListItem>
     ));
 
     return (
         <div className={styles.grid}>
             <Logo />
-            <div className={styles.sections}>{sections}</div>
+            <nav className={styles.sections}>
+                <UnorderedList>{sections}</UnorderedList>
+            </nav>
         </div>
     );
 }
--- src/MainPackage/components/Header.tsx
+++ src/MainPackage/components/Header.tsx
@@ -1,18 +1,23 @@
 import React, { ReactElement } from 'react';
-
-import { Link } from '../../UIKItPackage/components/Link';
 import { SectionsEnum } from '../enums/SectionsEnum';
+import { Link } from '../../UIKItPackage/components/Link';
+import { ListItem } from '../../UIKItPackage/components/ListItem';
+import { UnorderedList } from '../../UIKItPackage/components/UnorderedList';
 
 import styles from './Header.pcss';
 
 export function Header(): ReactElement {
     const sections = Object.values(SectionsEnum).map(name => (
-        <Link key={name} name={name} href="#section" />
+        <ListItem key={name} showMarker={false}>
+            <Link name={name} href="#section" />
+        </ListItem>
     ));
 
     return (
         <header className={styles.header}>
-            <nav className={styles.navigation}>{sections}</nav>
+            <nav className={styles.navigation}>
+                <UnorderedList>{sections}</UnorderedList>
+            </nav>
         </header>
     );
 }
